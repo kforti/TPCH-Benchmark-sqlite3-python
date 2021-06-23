df9068512365444d58813b098bb9f79a98473df2
kevin <kevin.r.fortier@gmail.com>
2021-06-22 20:28:23 -0400

    add requirements.txt, cleanup unused files, fix graph

--- build_graph.py
+++ build_graph.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 df = pd.read_csv('query_times_finals.csv')
 
 df['log_time'] = np.log2(df['time'])
+ndf = df.drop(['records_returned', 'time'], axis=1)
 
-bars = df['log_time'].plot.bar(x='name')
+bars = ndf.plot.bar(x='name')
 fig = bars.get_figure()
+params = {'mathtext.default': 'regular' }
+plt.rcParams.update(params)
+fig.suptitle("TPCH Query Run Times ($log_{2}$)", fontsize=16)
+plt.ylabel('Run Time ($log_{2}$)')
+plt.xlabel('Query Name')
+plt.gcf().subplots_adjust(bottom=0.33)
 fig.savefig("log_plot.png")
