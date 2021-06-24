2e05e904a1b96353142ad8837dce0985476d1c76
kevin <kevin.r.fortier@gmail.com>
2021-06-23 22:57:38 -0400

    update build_graph

---
+++ analysis.py
@@ -0,0 +1,7 @@
+
+import pandas as pd
+
+
+
+df = pd.read_csv('query_times_finals.csv')
+print(df.describe())
--- build_graph.py
+++ build_graph.py
@@ -3,17 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-df = pd.read_csv('query_times_finals.csv')
+# df = pd.read_csv('query_times_finals.csv')
+#
+# df['log_time'] = np.log2(df['time'])
+df = pd.read_csv('optimized_query_17.csv')
+ndf = df.drop(['records_returned'], axis=1)
 
-df['log_time'] = np.log2(df['time'])
-ndf = df.drop(['records_returned', 'time'], axis=1)
 
-bars = ndf.plot.bar(x='name')
-fig = bars.get_figure()
-params = {'mathtext.default': 'regular' }
-plt.rcParams.update(params)
-fig.suptitle("TPCH Query Run Times ($log_{2}$)", fontsize=16)
-plt.ylabel('Run Time ($log_{2}$)')
-plt.xlabel('Query Name')
-plt.gcf().subplots_adjust(bottom=0.33)
-fig.savefig("log_plot.png")
+def build_graph(df, save_location, x, xlabel, ylabel, title):
+
+    bars = df.plot.bar(x=x)
+    fig = bars.get_figure()
+    params = {'mathtext.default': 'regular' }
+    plt.rcParams.update(params)
+    fig.suptitle(title, fontsize=16)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.gcf().subplots_adjust(bottom=0.33)
+    fig.savefig(save_location)
+
+# build_graph(ndf, "log_plot.png", 'name', 'Query Name', 'Run Time ($log_{2}$)', "TPCH Query Run Times ($log_{2}$)")
+build_graph(ndf, "query17_plot.png", 'name', 'Query Name', 'Run Time', "TPCH Query 17 Run Times")
